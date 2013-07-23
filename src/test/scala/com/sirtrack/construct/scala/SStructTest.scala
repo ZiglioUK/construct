9ba47c62996a3ca88a97e850b6bfba3ca31bfb61
ZiglioNZ <zigliolie@sirtrack.com>
2013-07-23 16:32:05 +1200

    Renamed static method to avoid ambiguity

--- src/test/scala/com/sirtrack/construct/scala/SStructTest.scala
+++ src/test/scala/com/sirtrack/construct/scala/SStructTest.scala
@@ -1,10 +1,11 @@
 package com.sirtrack.construct.scala
 import com.sirtrack.construct.Core.Struct
 import com.sirtrack.construct.Core.ByteArray
-import com.sirtrack.construct.Core.Container
+import com.sirtrack.construct.Core.{ Container=> C }
 import com.sirtrack.construct.Macros._
 import org.junit._
 import org.junit.Assert._
+import com.sirtrack.construct.lib.Containers.Container
 
 @Test
 class SStructTest {
@@ -34,32 +35,32 @@ class SStructTest {
   @Test
   def structTest() {
       var struct = Struct( "struct", UBInt8("a"), UBInt16("b") )
-      var ca = struct.parse( ByteArray(1,0,2) )
-      var cb = Container( "a", 1:Integer, "b", 2:Integer )
+      var ca = struct.parse( ByteArray(1,0,2) ) : Container
+      var cb = C( "a", 1:Integer, "b", 2:Integer )
       assertTrue( ca.equals(cb) )
 
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), 
                Struct( "foo", UBInt8("c"), UBInt8("d") ))
       ca = struct.parse( ByteArray(1,0,2,3,4) )
-      cb = Container( "a",1:Integer, "b",2:Integer, "foo", Container( "c",3: Integer,"d",4: Integer))
+      cb = C( "a",1:Integer, "b",2:Integer, "foo", C( "c",3: Integer,"d",4: Integer))
       assertTrue( ca.equals(cb) )
 
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"))
-      var ba = struct.build( Container( "a", 1:Integer, "b", 2:Integer))
+      var ba = struct.build( C( "a", 1:Integer, "b", 2:Integer))
       assertArrayEquals( ByteArray(1,0,2), ba )
 
       var foo = Struct( "foo", UBInt8("c"), UBInt8("d") )
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), foo )
-      ba = struct.build( Container( "a",1:Integer, "b", 2:Integer, "foo", Container("c", 3:Integer, "d",4:Integer)))
+      ba = struct.build( C( "a",1:Integer, "b", 2:Integer, "foo", C("c", 3:Integer, "d",4:Integer)))
       assertArrayEquals( ByteArray(1:Integer,0,2:Integer,3,4), ba )
       
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), Embedded( Struct("foo", UBInt8("c"), UBInt8("d"))))
       ca = struct.parse( ByteArray(1:Integer,0,2:Integer,3,4) )
-      cb = Container( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer )
+      cb = C( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer )
       assertEquals( cb, ca )
 
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), Embedded( Struct("foo", UBInt8("c"), UBInt8("d"))))
-      ba = struct.build( Container( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer ))
+      ba = struct.build( C( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer ))
       assertArrayEquals( ByteArray(1:Integer,0,2:Integer,3,4), ba )
   }
 }
