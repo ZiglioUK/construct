ac4e49437fbe321adb32bdae11e011bf06083f5c
ZiglioNZ <zigliolie@sirtrack.com>
2013-07-24 13:45:15 +1200

    Added factory method for Containers, containing implicit conversion from Scala Int to Java Integer

--- src/test/scala/com/sirtrack/construct/scala/SStructTest.scala
+++ src/test/scala/com/sirtrack/construct/scala/SStructTest.scala
@@ -1,7 +1,8 @@
 package com.sirtrack.construct.scala
 import com.sirtrack.construct.Core.Struct
 import com.sirtrack.construct.Core.ByteArray
-import com.sirtrack.construct.Core.{ Container=> C }
+import com.sirtrack.construct.Core.FormatField
+//import com.sirtrack.construct.Core.{ Container=> C }
 import com.sirtrack.construct.Macros._
 import org.junit._
 import org.junit.Assert._
@@ -11,14 +12,6 @@ import com.sirtrack.construct.lib.Containers.Container
 class SStructTest {
   
 //  /**
-//  * @return unsigned, big endian 8-bit integer
-//  */
-//  case class UBInt8(name: String ) extends FormatField(name, '>', 'B')
-//
-//  /**
-//  * @return unsigned, big endian 16-bit integer
-//  */
-//  case class UBInt16(name: String) extends FormatField( name, '>', 'H' )
 //
 //  /**
 // * embeds a struct into the enclosing struct.
@@ -26,73 +19,119 @@ class SStructTest {
 // * @return
 // */
 //  case class Embedded( subcon: Construct ) extends Reconfig( subcon.name, subcon, subcon.FLAG_EMBED, 0 );
-
-    @Test
-    def testOK() {
-      assertTrue(true)
-    }
+  
+  object C{
+    implicit def javaToScalaInt(d: java.lang.Integer) = d.intValue
+    implicit def scalaToJavaInteger(d: Int ) = d.asInstanceOf[java.lang.Integer]
+    def apply(pairs: Any*) = new Container(pairs)
+  } 
 
   @Test
   def structTest() {
       var struct = Struct( "struct", UBInt8("a"), UBInt16("b") )
       var ca = struct.parse( ByteArray(1,0,2) ) : Container
-      var cb = C( "a", 1:Integer, "b", 2:Integer )
+      var cb = C( "a", 1, "b", 2 )
       assertTrue( ca.equals(cb) )
 
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), 
                Struct( "foo", UBInt8("c"), UBInt8("d") ))
       ca = struct.parse( ByteArray(1,0,2,3,4) )
-      cb = C( "a",1:Integer, "b",2:Integer, "foo", C( "c",3: Integer,"d",4: Integer))
+      cb = C( "a",1, "b",2, "foo", C( "c",3: Integer,"d",4: Integer))
       assertTrue( ca.equals(cb) )
 
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"))
-      var ba = struct.build( C( "a", 1:Integer, "b", 2:Integer))
+      var ba = struct.build( C( "a", 1, "b", 2))
       assertArrayEquals( ByteArray(1,0,2), ba )
 
       var foo = Struct( "foo", UBInt8("c"), UBInt8("d") )
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), foo )
-      ba = struct.build( C( "a",1:Integer, "b", 2:Integer, "foo", C("c", 3:Integer, "d",4:Integer)))
-      assertArrayEquals( ByteArray(1:Integer,0,2:Integer,3,4), ba )
+      ba = struct.build( C( "a",1, "b", 2, "foo", C("c", 3, "d",4)))
+      assertArrayEquals( ByteArray(1,0,2,3,4), ba )
       
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), Embedded( Struct("foo", UBInt8("c"), UBInt8("d"))))
-      ca = struct.parse( ByteArray(1:Integer,0,2:Integer,3,4) )
-      cb = C( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer )
+      ca = struct.parse( ByteArray(1,0,2,3,4) )
+      cb = C( "a", 1, "b", 2, "c", 3, "d", 4 )
       assertEquals( cb, ca )
 
       struct = Struct( "struct", UBInt8("a"), UBInt16("b"), Embedded( Struct("foo", UBInt8("c"), UBInt8("d"))))
-      ba = struct.build( C( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer ))
-      assertArrayEquals( ByteArray(1:Integer,0,2:Integer,3,4), ba )
+      ba = struct.build( C( "a", 1, "b", 2, "c", 3, "d", 4 ))
+      assertArrayEquals( ByteArray(1,0,2,3,4), ba )
   }
 
   @Test
   def sstructTest() {
       var struct = new SStruct( "struct", UBInt8("a"), UBInt16("b") )
       var ca = struct.parse( ByteArray(1,0,2) ) : Container
-      var cb = C( "a", 1:Integer, "b", 2:Integer )
+      var cb = C( "a", 1, "b", 2 )
       assertTrue( ca.equals(cb) )
 
       struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), 
                SStruct( "foo", UBInt8("c"), UBInt8("d") ))
       ca = struct.parse( ByteArray(1,0,2,3,4) )
-      cb = C( "a",1:Integer, "b",2:Integer, "foo", C( "c",3: Integer,"d",4: Integer))
+      cb = C( "a",1, "b",2, "foo", C( "c",3: Integer,"d",4: Integer))
       assertTrue( ca.equals(cb) )
 
       struct = SStruct( "struct", UBInt8("a"), UBInt16("b"))
-      var ba = struct.build( C( "a", 1:Integer, "b", 2:Integer))
+      var ba = struct.build( C( "a", 1, "b", 2))
       assertArrayEquals( ByteArray(1,0,2), ba )
 
       var foo = SStruct( "foo", UBInt8("c"), UBInt8("d") )
       struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), foo )
-      ba = struct.build( C( "a",1:Integer, "b", 2:Integer, "foo", C("c", 3:Integer, "d",4:Integer)))
-      assertArrayEquals( ByteArray(1:Integer,0,2:Integer,3,4), ba )
+      ba = struct.build( C( "a",1, "b", 2, "foo", C("c", 3, "d",4)))
+      assertArrayEquals( ByteArray(1,0,2,3,4), ba )
       
       struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), Embedded( SStruct("foo", UBInt8("c"), UBInt8("d"))))
-      ca = struct.parse( ByteArray(1:Integer,0,2:Integer,3,4) )
-      cb = C( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer )
+      ca = struct.parse( ByteArray(1,0,2,3,4) )
+      cb = C( "a", 1, "b", 2, "c", 3, "d", 4 )
       assertEquals( cb, ca )
 
       struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), Embedded( SStruct("foo", UBInt8("c"), UBInt8("d"))))
-      ba = struct.build( C( "a", 1:Integer, "b", 2:Integer, "c", 3:Integer, "d", 4:Integer ))
-      assertArrayEquals( ByteArray(1:Integer,0,2:Integer,3,4), ba )
+      ba = struct.build( C( "a", 1, "b", 2, "c", 3, "d", 4 ))
+      assertArrayEquals( ByteArray(1,0,2,3,4), ba )
   }
+ 
+
+  @Test
+  def caseClasssesSStructTest() {
+    
+  /*
+      * @return unsigned, big endian 8-bit integer
+      */
+      class UBInt8 extends FormatField(null, '>', 'B')
+      /**
+      * @return unsigned, big endian 16-bit integer
+      */
+      class UBInt16 extends FormatField( null, '>', 'H' )
+    
+      case class CCStruct( a: UBInt8, b:UBInt16 ) extends SStruct(null, a, b)
+      
+//      var ca = struct.parse( ByteArray(1,0,2) ) : Container
+//      var cb = C( "a", 1, "b", 2 )
+//      assertTrue( ca.equals(cb) )
+
+//      struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), 
+//               SStruct( "foo", UBInt8("c"), UBInt8("d") ))
+//      ca = struct.parse( ByteArray(1,0,2,3,4) )
+//      cb = C( "a",1, "b",2, "foo", C( "c",3: Integer,"d",4: Integer))
+//      assertTrue( ca.equals(cb) )
+//
+//      struct = SStruct( "struct", UBInt8("a"), UBInt16("b"))
+//      var ba = struct.build( C( "a", 1, "b", 2))
+//      assertArrayEquals( ByteArray(1,0,2), ba )
+//
+//      var foo = SStruct( "foo", UBInt8("c"), UBInt8("d") )
+//      struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), foo )
+//      ba = struct.build( C( "a",1, "b", 2, "foo", C("c", 3, "d",4)))
+//      assertArrayEquals( ByteArray(1,0,2,3,4), ba )
+//      
+//      struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), Embedded( SStruct("foo", UBInt8("c"), UBInt8("d"))))
+//      ca = struct.parse( ByteArray(1,0,2,3,4) )
+//      cb = C( "a", 1, "b", 2, "c", 3, "d", 4 )
+//      assertEquals( cb, ca )
+//
+//      struct = SStruct( "struct", UBInt8("a"), UBInt16("b"), Embedded( SStruct("foo", UBInt8("c"), UBInt8("d"))))
+//      ba = struct.build( C( "a", 1, "b", 2, "c", 3, "d", 4 ))
+//      assertArrayEquals( ByteArray(1,0,2,3,4), ba )
+ }
+  
 }
